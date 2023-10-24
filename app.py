67e28e2a4dbce70ab504ab14a88023e3a62ab55f
YourUsername <youremail@example.com>
2023-10-24 16:59:34 +0530

    added doc

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-# two decorators, same function
+# Existing routes
 @app.route('/')
 @app.route('/index.html')
 def index():
     return render_template('index.html', the_title='E-Commerce App')
+
+
+@app.route('/products')
+def products():
+    return "Check out our latest products."
+
+# New route for calculation, This is just for fun  : http://127.0.0.1/calculate/10/20
+@app.route('/calculate/<int:num1>/<int:num2>')
+def calculate(num1, num2):
+    result = num1 + num2  # You can replace this with your own calculation
+    return f"The result of {num1} + {num2} is {result}."
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
